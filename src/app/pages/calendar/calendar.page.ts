6f8e2a5d3bacb3304df4d58eb62a6aae4dbea768
joselopez59 <50359100+joselopez59@users.noreply.github.com>
2020-07-02 18:31:18 +0200

    1

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -15,15 +15,38 @@ export class CalendarPage implements OnInit {
   //   pickMode: 'range'
   // };
 
-  date: {
-    from: string
-    to: string
-  } = {
-    from: '2018-06-28',
-    to: '2018-06-30'
+  options: CalendarComponentOptions = {
+    showToggleButtons: true,
+    from: new Date(2000, 0, 1),
+    pickMode: 'multi'
   };
-  
-  
+
+  optionsMulti: CalendarComponentOptions = {
+    from: new Date(2000, 0, 1),
+    pickMode: 'range',
+    daysConfig: [
+      {
+      date: new Date(2020, 5, 14),
+      title: 'title',
+      subTitle: 'subtitle',
+      marked: true,
+      },
+      {
+        date: new Date(2020, 5, 16),
+        title: 'title',
+        subTitle: 'subtitle',
+        marked: true
+        }
+
+    ]
+  };
+
+  date: string[] = ['2020-06-28', '2020-06-30'];
+
+  // dateRange: {
+  //   from: string
+  //   to: string
+  // } = { from: '2018-06-28', to: '2018-06-29' };
   
   type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
   
