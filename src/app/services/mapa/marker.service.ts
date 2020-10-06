f8e9da47bd588a2b186b54a45d9d50beb0dc3daf
joselopez59 <50359100+joselopez59@users.noreply.github.com>
2020-10-06 15:19:06 +0200

    1

--- src/app/services/mapa/marker.service.ts
+++ src/app/services/mapa/marker.service.ts
@@ -4,6 +4,8 @@ import { MapaPageModule } from 'src/app/pages/mapa/mapa.module';
 
 // import { environment } from '../../../environments/environment';
 import * as L from 'leaflet';
+import { PopUpService } from './pop-up.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,23 +19,28 @@ export class MarkerService {
 
   pois: string = '/assets/map/pois.json';
 
-  constructor( private http: HttpClient ) {}
+  constructor( 
+    private http: HttpClient,
+    private PopUpService
+  ) {}
 
   makePoisMarkers(map: L.map): void {
     this.http.get(this.pois).subscribe ((res: any) => {
-      //console.log("get", res.features);
       
-      // for (const c of res.features) { 
-      //   console.log(c);
-      // }
       
       for (const c of res.features) {
-        
+        console.log(c);
         const lat = c.geometry.coordinates[0];
-        console.log("lat", lat);
+        //console.log("lat", lat);
         const lon = c.geometry.coordinates[1];
-        console.log("lon", lon);
-        const marker = L.marker([lon, lat]).addTo(map);
+        //console.log("lon", lon);
+
+        const marker = L.marker([lon, lat]);
+
+        //marker.bindPopup(this.PopUpService.makePopup(c));
+
+        //marker.addTo(map);
+
       }
 
     });
--- src/app/services/mapa/pop-up.service.ts
+++ src/app/services/mapa/pop-up.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PopUpService {
 
   constructor() { }
+
+  makePopup(data: any): string {
+    return "" + 
+      "<div>Name: ${ data.name }</div>"
+  }
 }
