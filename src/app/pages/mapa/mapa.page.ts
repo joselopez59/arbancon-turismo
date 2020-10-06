ef485c4ed1a370ad472409df2514d5da0daca9a9
joselopez59 <50359100+joselopez59@users.noreply.github.com>
2020-10-06 11:49:36 +0200

    2

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+//import * as Mapkey from 'lib/leaflet-mapkey-icon/dist/L.Icon.Mapkey.js'
+
+declare function Mapkey(options: any);
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
+
 export class MapaPage {
 
-  icon = {
-    icon: L.icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 0 ],
-      // specify the path here
-      iconUrl: '/marker-icon.png',
-      shadowUrl: '/marker-shadow.png'
-   })
-  };
-
+  // l_icon = L.icon;
+  
+  // l_icon = Mapkey(
+  //   {
+  //     icon:"school",
+  //     color:'#725139',
+  //     background:'#f2c357',
+  //     size:30
+  //   }
+  // );
+  
+  
   map: L.Map;
 
   constructor() { }
@@ -46,12 +52,36 @@ export class MapaPage {
       maxZoom: 20
     });
 
-    this.map = new L.Map('mapId').setView([40.965, -3.115], 16);
+    // this.map = new L.Map('mapId').setView([40.965, -3.115], 18);
+    this.map = new L.Map('mapId').setView([40.96404, -3.11249 ], 19);
 
     // this.createLayer().addTo(this.map);
-    
 
-    const marker = L.marker([40.96404, -3.11249], this.icon ).addTo(this.map);
+    // this.l_icon = L.icon.mapkey(
+    //   {
+    //     icon:"school",
+    //     color:'#725139',
+    //     background:'#f2c357',
+    //     size:30
+    //   }
+    // );
+
+    const icon = L.icon ({
+      
+        
+      // specify the path here
+      iconUrl: '/marker-icon.png',
+      shadowUrl: '/marker-shadow.png'
+     });
+
+    //const marker = L.marker([40.96404, -3.11249], icon).addTo(this.map);
+
+    //var mapkey = Mapkey({icon:"school",color:'#725139',background:'#f2c357',size:30});
+
+    L.marker(
+      [ 40.96404, -3.11249 ],
+      {icon: icon})
+      .addTo(this.map);
 
     callejero.addTo(this.map);
     // topografico.addTo(this.map);
